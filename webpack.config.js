d7d94ed782e8e3ba4bb79c57b1d692086f50ab8d
ielow <pako.adn@gmail.com>
2017-09-27 22:24:54 -0500

    babel & typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const path= require('path');
-const HTMLWebpackPlugin= require('html-webpack-plugin');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports={
+module.exports = {
   entry: './app/index.ts',
-  output:{
-    filename:'bundle.js',
-    path: path.resolve(__dirname,'dist')
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-        resolve:{
-        extensions: [".ts",".js"]
-        },
-        module:{
-           rules:[
-              {
-                  test:/\.pug$/, 
-                  exclude:['node_modules'],     
-                  loaders:['html-loader','pug-html-loader']
-              },
-              {
-                test:/\.ts$/,
-                exclude:['node_modules'],
-                loader:'ts-loader'
-              }
-              
-             ]
-         },
-  plugins:[
-    new HTMLWebpackPlugin({
-      title:'Scaffolding',
-      hash:true,
-      template:'./app/views/index.pug',
-      exclude:['node_modules','views/layout']
-    })
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: ['node_modules'],
+        loaders: ['html-loader', 'pug-html-loader']
+      },
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: ['babel-loader', 'ts-loader']
+      }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".js",".pug"]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Scaffolding',
+      hash: true,
+      template: './app/views/index.pug',
+      exclude: ['node_modules', 'views/layout']
+    })
+  ]
 }
